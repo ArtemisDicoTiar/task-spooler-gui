0186387dca674c23287e22d3fa42b1f768f4b86d
jongyoonkim <johnjongyoonkim@gmail.com>
2025-03-27 14:50:48 +0900

    command parsing bug fixed

--- src/task_spooler_gui/task_spooler_utils.py
+++ src/task_spooler_gui/task_spooler_utils.py
@@ -48,21 +48,13 @@ def parse_tasklist_to_json(input_str):
                 d["Label"], d["Command"] = m.groups()
 
             ts_id = d["ID"]
-            d["Command"] = get_command_output(ts_id)
+            # d["Command"] = get_command_output(ts_id)
 
         else:
             d["Label"] = None
 
     # Convert to JSON
     return all_parsed_data
-
-
-def get_command_output(job_id, socket_name=None):
-    env = get_env(socket_name)
-    output = subprocess.check_output(
-        f"{TASK_SPOOLER_CMD} -F {job_id}", env=env, shell=True, encoding="utf-8"
-    )
-    return output
 
 
 def list_jobs(socket_name=None):
